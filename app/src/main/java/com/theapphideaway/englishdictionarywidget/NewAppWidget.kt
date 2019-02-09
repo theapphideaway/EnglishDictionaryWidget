5fdc8accad0fe54923f8b8d4c3302a375e0f995b
ianschoenrock <theapphideaway@gmail.com>
2019-02-09 13:59:40 -0500

    Initial commit

---
+++ app/src/main/java/com/theapphideaway/englishdictionarywidget/NewAppWidget.kt
@@ -0,0 +1,57 @@
+package com.theapphideaway.englishdictionarywidget
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+import android.content.Intent
+
+
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class NewAppWidget : AppWidgetProvider() {
+
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    companion object {
+
+        internal fun updateAppWidget(
+            context: Context, appWidgetManager: AppWidgetManager,
+            appWidgetId: Int
+        ) {
+
+            val widgetText = context.getString(R.string.appwidget_text)
+
+            // Construct the RemoteViews object
+            val views = RemoteViews(context.packageName, R.layout.new_app_widget)
+
+            val mainIntent = Intent(context, MainActivity::class.java)
+
+            val pendingIntent = PendingIntent.getBroadcast(context, 0, mainIntent, PendingIntent.FLAG_ONE_SHOT)
+
+            views.setOnClickPendingIntent(R.id.linear_layout, PendingIntent.getActivity(context, 0, mainIntent, 0))
+
+
+
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+    }
+}
+
